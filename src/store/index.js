46ae7243071788eec17123f1a9519547ba50d4b5
Rafael Dell'Aquila <rafael@dellaquila.dev>
2021-12-17 16:54:57 -0300

    :alien: call the api and populating the UI

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import { createStore } from 'vuex'
+import axios from 'axios'
+export default createStore({
+  state: {
+    products: [],
+    productsInBag: []
+  },
+  mutations: {
+    loadProducts(state, products) {
+      state.products = products
+    },
+
+    addToBag(state, product) {
+      state.productsInBag.push(product)
+    },
+
+    removeFromBag(state, productId) {
+      const updatedBag = state.productsInBag.filter(item => productId != item.id)
+      state.productsInBag = updatedBag
+    }
+  },
+  actions: {
+    loadProducts({commit}) {
+      axios.get('https://fakestoreapi.com/products').then((res) => {
+        commit('loadProducts', res.data)
+      })
+    },
+
+    addToBag({commit},  product) {
+      commit('addToBag', product)
+    },
+
+    removeFromBag({commit},  productId) {
+      commit('removeFromBag', productId)
+    }
+  },
+  modules: {}
+})
